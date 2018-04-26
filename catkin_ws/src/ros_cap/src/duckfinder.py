37d2e902d3d134d74c43e98626a845b2f6a1556d
Andres <andres001999@gmail.com>
2018-04-26 20:13:53 -0300

    Se termino el duckfinder y se agrego la calibracion de la camara

--- catkin_ws/src/ros_cap/src/duckfinder.py
+++ catkin_ws/src/ros_cap/src/duckfinder.py
@@ -5,16 +5,25 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import numpy as np
-
+from geometry_msgs.msg import Point
+fx=162.16
+fy=164
+cx=162.94
+cy=120
+dpx=3.5
+dpy=3.0
+dpeligro=10
 class duckfinder(object):
 	def __init__(self):
 		super(duckfinder, self).__init__() 
-		self.subscriber=rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
-		self.publisher=rospy.Publisher("/usb_cam/mask", Image,queue_size=1)
-		self.publisher2=rospy.Publisher("/usb_cam/duck", Image,queue_size=1)
-		self.publisher3=rospy.Publisher("/usb_cam/t1", Image,queue_size=1)
+		self.subscriber=rospy.Subscriber("/duckiebot/camera_node/image/rect",Image,self.callback)
+		#self.publisher=rospy.Publisher("/usb_cam/mask", Image,queue_size=1) 
+		#self.publisher2=rospy.Publisher("/usb_cam/duck", Image,queue_size=1)
+		#self.publisher3=rospy.Publisher("/usb_cam/t1", Image,queue_size=1)
 		self.publisher4=rospy.Publisher("/usb_cam/rect", Image,queue_size=1)
+		self.publisher=rospy.Publisher("posicion", Point,queue_size=1)
 		self.bridge=CvBridge()
+		self.point=Point()
 		
 
 
@@ -24,8 +33,7 @@ class duckfinder(object):
 		color_space= cv2.COLOR_BGR2HSV
 		image_out=cv2.cvtColor(image, color_space)
 		
-		mask= cv2.inRange(image_out,np.array([20,40,100]),np.array([60,255,255]))
-		#image_out=cv2.bitwise_and(image, image, mask=mask)		
+		mask= cv2.inRange(image_out,np.array([15,150,150]),np.array([45,255,255]))		
 		msg=self.bridge.cv2_to_imgmsg(mask,"mono8")
 		image_out=cv2.bitwise_and(image,image,mask=mask)
 		msg2=self.bridge.cv2_to_imgmsg(image_out,"bgr8")
@@ -34,16 +42,27 @@ class duckfinder(object):
 		msg3=self.bridge.cv2_to_imgmsg(image_out2,"bgr8")
 		(_,contours, hierarchy) = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 		image_out3=image.copy()
+		L=[0,0,0]
 		for pic, contour in enumerate(contours):
 			area=cv2.contourArea(contour)
-			if (area>500):
+			if (area>100):
 				x,y,w,h = cv2.boundingRect(contour)
 				image_out3=cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
+				u=x+w/2.0
+				v=y+h/2.0
+				z=(fy*dpy*1.0)/h
+				x3d=((u-cx)*z)/fx
+				y3d=((v-cy)*z)/fy
+				L=[x3d,y3d,z]
+				self.point.x=x3d
+				self.point.y=y3d
+				self.point.z=z
+		self.publisher.publish(self.point)
 		msg4=self.bridge.cv2_to_imgmsg(image_out3,"bgr8")
 		self.publisher4.publish(msg4)
-		self.publisher.publish(msg)
-		self.publisher2.publish(msg2)
-		self.publisher3.publish(msg3)
+		#self.publisher.publish(msg)
+		#self.publisher2.publish(msg2)
+		#self.publisher3.publish(msg3)
 		#self.publisher4.publish(msg4)
 		
 
